f075444509590c02bbb9c378278c03251e15801c
logan4 <loganliebsch@utexas.edu>
2017-12-12 22:37:36 +0000

    start arbitrage_trader

---
+++ arbitrage_trader.py
@@ -0,0 +1,47 @@
+from gdax.order_book import OrderBook
+import sys
+import time
+import datetime as dt
+
+from gdax.public_client import PublicClient
+
+if __name__ == '__main__':
+	
+	'''
+	class ArbitrageTrader(EthUsdOrderBook, EthBtcOrderBook, BtcUsdOrderBook):
+
+		def __init__(self, product_id = ''):
+			super(ArbitrageTrader, self).__init__(product_id=product_id)
+
+			self._EthUsd = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
+			self._EthBtc = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
+			self._BtcUsdBid = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
+
+		def on_message(self, message):
+			super(ArbitrageTrader, self).on_message(message)
+	'''
+
+	ethusd = OrderBook('ETH-USD')
+	ethusd.start()
+	ethbtc = OrderBookConsole('ETHBTC')
+	ethbtc.start()
+	btcusd = OrderBookConsole('BTCUSD')
+	btcusd.start()
+
+
+	#arbitrage logic to buy ETHUSD
+	'''
+	if ethusd.bid/ethbtc.bid == x:
+		if 	
+	'''
+
+	try:
+        	while True:
+            		time.sleep(10)
+    	except KeyboardInterrupt:
+        	order_book.close()
+
+    	if order_book.error:
+        	sys.exit(1)
+    	else:
+        	sys.exit(0)
