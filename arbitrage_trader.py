12f9e6397885445a446a2e8edd8f7a81ea3a2446
logan4 <loganliebsch@utexas.edu>
2017-12-12 23:51:11 +0000

    add spread class

--- arbitrage_trader.py
+++ arbitrage_trader.py
@@ -7,43 +7,64 @@ from gdax.public_client import PublicClient
 
 if __name__ == '__main__':
 	
-	'''
-	class ArbitrageTrader(EthUsdOrderBook, EthBtcOrderBook, BtcUsdOrderBook):
+	
+	class Spread(OrderBook):
 
 		def __init__(self, product_id = ''):
-			super(ArbitrageTrader, self).__init__(product_id=product_id)
+			super(Spread, self).__init__(product_id=product_id)
 
-			self._EthUsd = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
-			self._EthBtc = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
-			self._BtcUsdBid = {"bid" : None, "bidDepth" : None, "ask" : None, "askDepth": None}
+			self._bid = None
+			self._ask = None
+			self._bidDepth = None
+			self._askDepth = None
 
 		def on_message(self, message):
-			super(ArbitrageTrader, self).on_message(message)
-	'''
+			super(Spread, self).on_message(message)
+			
+			bid = self.get_bid()
+			bidDepth = sum([b['size'] for b in self.get_bids(bid)])
+			ask = self.get_ask()
+			askDepth = sum([a['size'] for a in self.get_asks(ask)])
+
+			if self._bid == bid and self._ask == ask and self._bidDepth == bidDepth and self._askDepth == askDepth:
+                		# If there are no changes to the bid-ask spread since the last update, no need to print
+                		pass
+            		else:
+                		# If there are differences, update the cache
+                		self._bid = bid
+                		self._ask = ask
+                		self._bidDepth = bidDepth
+                		self._askDepth = askDepth
+                		print('{} {} bid: {:.3f} @ {:.2f}\task: {:.3f} @ {:.2f}'.format(
+                    			dt.datetime.now(), self.product_id, bidDepth, bid, askDepth, ask))
+	
 	# will add support for other pairs
 	#first version will be designed for buying ETH/USD to BTC/USD only
-	ethusd = OrderBook('ETH-USD')
+	ethusd = Spread('ETH-USD')
 	ethusd.start()
-	ethbtc = OrderBook('ETH-BTC')
+	# wait 4 seconds due to websocket rate limit
+	ethbtc = Spread('ETH-BTC')
 	ethbtc.start()
-	btcusd = OrderBook('BTC-USD')
+	btcusd = Spread('BTC-USD')
 	btcusd.start()
 
-
+	
 	#arbitrage logic to buy ETHUSD
 	'''
 	if ethusd.bid/ethbtc.bid == x:
 		if 	
 	'''
-	'''
+	
 	try:
         	while True:
             		time.sleep(10)
     	except KeyboardInterrupt:
-        	order_book.close()
+        	ethusd.close()
+		ethbtc.close()
+		btcusd.close()
 
-    	if order_book.error:
+    	if ethusd.error or ethbtc.error or btcusd.error:
         	sys.exit(1)
     	else:
         	sys.exit(0)
-	'''
+	
