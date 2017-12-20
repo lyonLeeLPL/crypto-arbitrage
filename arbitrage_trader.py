64dd11274a89cba0e6aa8ef01b51321eaa4dbefe
logan4 <loganliebsch@utexas.edu>
2017-12-20 20:15:56 +0000

    start support for ask data

--- arbitrage_trader.py
+++ arbitrage_trader.py
@@ -2,13 +2,15 @@ from gdax.order_book import OrderBook
 import sys
 import time
 import datetime as dt
-
+#consider changing to multiprocessing
 
 from gdax.public_client import PublicClient
 import gdax
 import json
 import decimal
 from Queue import Queue
+
+#keep stopping and starting aws instance until i get one that is closer to gdax server, got .6 ms on first try
 
 if __name__ == '__main__':
 	
@@ -42,9 +44,8 @@ if __name__ == '__main__':
                 		self._askDepth = askDepth
                 		print('{} {} bid: {:.3f} @ {:.2f}\task: {:.3f} @ {:.2f}'.format(
                     			dt.datetime.now(), self.product_id, bidDepth, bid, askDepth, ask))
-				#check_arbitrage()
-
-				q.put({"product":self.product_id, "bid":self._bid})
+				
+				q.put({"product":self.product_id, "bid":(self._bid, self._bidDepth), "ask":(self._ask, self._askDepth)})
 			 
 	q = Queue(maxsize=0)
 	num_threads = 3	
@@ -70,18 +71,22 @@ if __name__ == '__main__':
 	#check for arbitrage opportunity, currently assuming no fee
 	#well this works now write clean code and figure out how to call only when on_message is called so im not wasting resources, using 100% cpu
 
-	bids = {"BTC-USD": 0, "ETH-BTC": 0, "ETH-USD": 0} 
+	#store tuple (price,size)
+	bids = {"BTC-USD": None, "ETH-BTC": None, "ETH-USD": None} 
+	asks = {"BTC-USD": None, "ETH-BTC": None, "ETH-USD": None}
 	while True:
 		try:
-			pnl = bids['BTC-USD'] * bids['ETH-BTC'] - bids['ETH-USD']
+			if bids['BTC-USD'] is not None and bids['ETH-BTC'] is not None and bids['ETH-USD'] is not None:
+				pnl = bids['BTC-USD'][0] * bids['ETH-BTC'][0] - bids['ETH-USD'][0]
 			#pnl = btcusd._bid * ethbtc._bid - ethusd._ask
 			last_update = q.get()
 			bids[last_update['product']] = last_update['bid']
+			asks[last_update['product']] = last_update['ask']
 			print "PNL: ",pnl
 
 			#if pnl > x, then trade
 			#not using market making, will have to pay fees, not a profitable entry strategy
-			'''
+			
 			if pnl > 0:
 				#get margin account so these can be done in sync
 				order1_id = auth_client.buy(price=str(ethusd._ask), size='1.0', product_id = 'ETH-USD')
@@ -99,33 +104,24 @@ if __name__ == '__main__':
 				order3_id = auth_client.sell(price=str(btcusd._bid), size = btc_balance, product_id='BTC-USD')
 				time.sleep(60)
 				#just for test so we don't open a ton of trades repeatedly
-			'''				
+		
+		#except (KeyboardInterrupt, SystemExit):
+		#	ethusd.close()
+		#	ethbtc.close()
+		#	btcusd.close()
+		
 		except Exception as e:
-			print e
-	
-		#return pnl > 0  	
-	
+			print e 	
+		'''
 
-	
-	try:
-		#print btcusd._bid
-		#print ethbtc._bid
-		#print ethusd._ask
-		time.sleep(10)
-        	#while True:
-		#	pnl = btcusd._bid * ethbtc._bid - ethusd._ask
-		#	print "PNL: ",pnl
-			#print btcusd._bid
-			#print ethbtc._bid
-			#print ethusd._ask
-            		#time.sleep(10)
-    	except KeyboardInterrupt:
-        	ethusd.close()
-		ethbtc.close()
-		btcusd.close()
+		if ethusd.error or ethbtc.error or btcusd.error:
+			sys.exit(1)
+		else:
+			sys.exit(0)
+		'''
+	ethusd.close()
+	ethbtc.close()
+	btcusd.close()
 
-    	if ethusd.error or ethbtc.error or btcusd.error:
-        	sys.exit(1)
-    	else:
-        	sys.exit(0)
-	
+	#to avoid fees
+	#def maker(bids, asks):	
